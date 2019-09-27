e0419f8ebac213f218e0243ed48e2e898b3352d6
cwebrien <cameron@brien.ca>
2019-09-26 21:58:29 -0400

    Loop the guessing

--- rust-katas/guessing_game/src/main.rs
+++ rust-katas/guessing_game/src/main.rs
@@ -12,24 +12,33 @@ fn main()
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .ok()
-        .expect("failed to read line");
-
-    let guess: u32 = guess.trim().parse()
-        .ok()
-        .expect("Please type a number!");
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) 
+    loop
     {
-        Ordering::Less    => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal   => println!("You win!"),
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .ok()
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse() 
+        {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) 
+        {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => 
+            {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
