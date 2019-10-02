62b4887fa44b87fbc4ce9daa9e907dc3e529e075
cwebrien <cameron@brien.ca>
2019-10-01 20:27:24 -0400

    Small change to update loop

--- rust-katas/control_flow/src/main.rs
+++ rust-katas/control_flow/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+fn main() 
+{
+    let result: i64 = {
+        let mut counter = 4;
+        let mut a = 2;
+        while counter > 0
+        {
+            a = a * a;
+            counter = counter - 1;
+        }
+        a
+    };
+    println!("Result: {}", result);
 }
