a082b6ab73f18a1911ee61d5fc2d8fcd03353934
cwebrien <cameron@brien.ca>
2022-06-08 21:34:19 -0400

    Primitive implementation of add/mult on complex numbers

--- rust-katas/complex_numbers/src/main.rs
+++ rust-katas/complex_numbers/src/main.rs
@@ -3,16 +3,22 @@ fn main() {
     let b: (f64, f64) = (3.0, 2.0);
 
     let c = complex_add(a, b);
-    println!("({}, {}) + ({}, {}) = ({}, {})", a.0, a.1, b.0, b.1, c.0, c.1);
-    
+    println!(
+        "({}, {}) + ({}, {}) = ({}, {})",
+        a.0, a.1, b.0, b.1, c.0, c.1
+    );
+
     let c = complex_multiply(a, b);
-    println!("({}, {}) * ({}, {}) = ({}, {})", a.0, a.1, b.0, b.1, c.0, c.1);
+    println!(
+        "({}, {}) * ({}, {}) = ({}, {})",
+        a.0, a.1, b.0, b.1, c.0, c.1
+    );
 }
 
 fn complex_add(a: (f64, f64), b: (f64, f64)) -> (f64, f64) {
     return (a.0 + b.0, a.1 + b.1);
-} 
+}
 
 fn complex_multiply(a: (f64, f64), b: (f64, f64)) -> (f64, f64) {
     return (a.0 * b.0 - a.1 * b.1, a.0 * b.1 + a.1 * b.0);
-} 
+}
