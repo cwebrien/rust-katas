bb7b2843fc4810c2e28de1aa34eb1d836350f011
Cameron Brien <cameron@brien.ca>
2022-07-10 14:07:11 -0400

    Implemented median and mode

--- rust-katas/vector/src/main.rs
+++ rust-katas/vector/src/main.rs
@@ -1,6 +1,41 @@
-fn main() {
-    let v = vec![1, 2, 3, 4, 5];
+/*
+Given a list of integers, use a vector and return the median (when sorted, the value in the middle position)
+and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+*/
 
- //   let does_not_exist = &v[100];
-    let does_not_exist = v.get(100);
+use std::collections::HashMap;
+
+fn median(v: &Vec<i32>) -> i32 {
+    let mut v = v.clone();
+    v.sort();
+    
+    return match v.get(v.len() / 2) {
+        Some(median) => *median,
+        None => 0
+    }
+}
+
+fn mode(v: &Vec<i32>) -> i32 {
+    let mut counts: HashMap<i32, i32> = HashMap::new();
+    let mut current_mode: (i32, i32) = (0, 0); // (mode, mode_count)
+
+    for i in v.iter() {
+        let count = counts.entry(*i).or_insert(0);
+        *count += 1;
+        if *count > current_mode.1 {
+            current_mode = (*i, *count);
+        }
+    }
+    
+    current_mode.0
+}
+
+fn main() {
+    let v = vec![2, 1, 3, 4, 3, 5, 7, 9];
+    
+    let median = median(&v);
+    let mode = mode(&v);
+
+    println!("Median: {}", median);
+    println!("Mode:   {}", mode);
 }
