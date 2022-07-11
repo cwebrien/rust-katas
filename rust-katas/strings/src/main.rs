32a5ed7447d78fa1a707b21d515feabae99c87f8
cwebrien <cameron@brien.ca>
2022-07-11 16:12:32 -0400

    Stub out the pig latin method

--- rust-katas/strings/src/main.rs
+++ rust-katas/strings/src/main.rs
@@ -1,3 +1,18 @@
+/*
+Convert strings to pig latin. The first consonant of each word is moved to
+the end of the word and “ay” is added, so “first” becomes “irst-fay.” Words
+that start with a vowel have “hay” added to the end instead (“apple” becomes
+“apple-hay”). Keep in mind the details about UTF-8 encoding!
+*/
+
+fn pig_latin(original: &String) -> String {
+    return String::from("to implement");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let to_convert = "Hello, my name is Cameron Brien";
+    let result = pig_latin(&to_convert.to_string());
+
+    println!("Original:  {}", to_convert);
+    println!("Converted: {}", result);
 }
