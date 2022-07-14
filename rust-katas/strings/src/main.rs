de62ddba1a66167664cd1dcaf64e5cbdf2e3df87
cwebrien <cameron@brien.ca>
2022-07-14 16:16:36 -0400

    Implement crude pig latin convertor

--- rust-katas/strings/src/main.rs
+++ rust-katas/strings/src/main.rs
@@ -5,12 +5,41 @@ that start with a vowel have “hay” added to the end instead (“apple” bec
 “apple-hay”). Keep in mind the details about UTF-8 encoding!
 */
 
+fn is_vowel(s: &str) -> bool {
+    match s {
+        "A" | "a" | "E" | "e" | "I" | "i" | "O" | "o" | "U" | "u" => true,
+        _ => false,
+    }
+}
+
 fn pig_latin(original: &String) -> String {
-    return String::from("to implement");
+    let mut result = String::new();
+
+    for word in original.split(" ") {
+        let first_char;
+        match word.get(0..1) {
+            Some(v) => first_char = v,
+            None => panic!(),
+        }
+
+        let mut pigified = String::from(word);
+        if is_vowel(&first_char) {
+            pigified.push_str("hay");
+        } else {
+            let removed = pigified.remove(0);
+            pigified.push(removed);
+            pigified.push_str("ay");
+        }
+
+        result.push_str(&pigified);
+        result.push(' ');
+    }
+
+    return result;
 }
 
 fn main() {
-    let to_convert = "Hello, my name is Cameron Brien";
+    let to_convert = "Hello my name is Cameron Brien";
     let result = pig_latin(&to_convert.to_string());
 
     println!("Original:  {}", to_convert);
