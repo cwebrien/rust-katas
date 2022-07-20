f35e7cf080e49f7f45e39807d09bfb617517199a
cwebrien <cameron@brien.ca>
2022-07-19 21:57:54 -0400

    Little hook to play with lifetimes

--- rust-katas/lifetimes/src/main.rs
+++ rust-katas/lifetimes/src/main.rs
@@ -1,3 +1,17 @@
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = "abc";
+    let longer;
+    let y = String::from("def");
+    {
+        longer = longest(&x, y.as_str());
+    }
+    println!("Longer: {}", longer);
 }
